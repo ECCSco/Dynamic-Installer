f6bb9a295a6f1fef609f16497ba011f3880019ff
ECCSco <134647048+ECCSco@users.noreply.github.com>
2025-02-16 19:37:46 +0300

    Update Dynamic Island V2.lua

--- Dynamic Island V2.lua
+++ Dynamic Island V2.lua
@@ -2264,34 +2264,25 @@ InstallerName.TextYAlignment = Enum.TextYAlignment.Bottom
 AddAppButton.MouseButton1Click:Connect(function() 
 InstallerFrame:TweenSize(UDim2.new(0, 551, 0, 285),"InOut","Sine",0.2) 
 end) 
-if isfolder("Dynamic_Island/Apps") then
-local files = listfiles("Dynamic_Island/Apps/")
-local fileCount = #files
-local loadedCount = 0
-
-for _, file in ipairs(files) do
+if isfolder("Dynamic_Island/Apps") then 
+local files = listfiles("Dynamic_Island/Apps/") 
+for _, file in ipairs(files) do 
 spawn(function()
-loadstring(readfile(file))()
-loadedCount = loadedCount + 1
+loadstring(readfile(file))() 
 end)
-end
-
+end 
 spawn(function()
-while wait(0.5) do
-if loadedCount == fileCount then
-for _, loadedapps in pairs(AppsScrollingFrame:GetChildren()) do
-if loadedapps:IsA("GuiObject") then
-AppsCorner = Instance.new("UICorner")
-AppsCorner.CornerRadius = UDim.new(0, 20)
-AppsCorner.Parent = loadedapps
+while wait(0.5) do    
+for _, loadedapps in pairs(AppsScrollingFrame:GetChildren()) do 
+if loadedapps:IsA("GuiObject") then 
+AppsCorner = Instance.new("UICorner") 
+AppsCorner.CornerRadius = UDim.new(0, 20) 
+AppsCorner.Parent = loadedapps 
+end 
 end
-end
-break
-end
-end
+end 
 end)
-end
-
+end 
 end
 
 spawn(function()
